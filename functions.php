3e615d05179e998929ad5042fec9167af6f167c7
Julien Bourdeau <julien@sigerr.org>
2012-07-22 14:51:12 +0200

    change boostrap css to LESS. Add detailled js lib. change the script enqueue function

--- functions.php
+++ functions.php
@@ -155,18 +155,16 @@ function _s_scripts() {
 	global $post;
 
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
-
-	if ( ENV == 'dev' && !is_admin() ) {
-		wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array( 'style' ) );
-		wp_enqueue_style( 'bootstrap-responsive', get_template_directory_uri() . '/css/bootstrap-responsive.css', array( 'style', 'bootstrap' ) );
-	}
 	
 	if ( defined('ENV') && !is_admin() ) {
 		wp_enqueue_style( 'style-dev', get_template_directory_uri() . '/css/style-'.ENV.'.css', array( 'style' ) );
 	}
 
 	wp_enqueue_script( 'small-menu', get_template_directory_uri() . '/js/small-menu.js', array( 'jquery' ), '20120206', true );
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array( 'jquery' ), '2.0.4', true );
+
+	if ( defined('ENV') && !is_admin() ) {
+		wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap-'.ENV.'.min.js', array( 'jquery' ), '2.0.4', true );
+	}
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
